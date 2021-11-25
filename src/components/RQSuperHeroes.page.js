3256af5026bc72bccd4b32fb255639060567a988
Akshay P <akshayp@colossusnexus.com>
2021-11-25 16:22:50 +0530

    fetching data with useQuery

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,23 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHero = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  return <div>RQ Super Heroes</div>;
+  const { data, isLoading } = useQuery("super-heroes", fetchSuperHero);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
